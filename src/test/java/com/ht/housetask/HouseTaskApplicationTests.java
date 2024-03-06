776bac43be70bd2898d18735322046f3da609127
Oswaldo Moo <moswaldo227@gmail.com>
2024-03-05 21:45:34 -0500

    first commit

---
+++ src/test/java/com/ht/housetask/HouseTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ht.housetask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
